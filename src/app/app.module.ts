f7ce38a4cada96f034b94ecbd602305168f1c35c
gimartinez <gm@famly.co>
2021-04-05 14:35:15 +0200

    Revert material update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,50 +17,16 @@ import {AuthService} from "./services/auth.service";
 import {QuestionService} from "./services/question.service";
 import {SocialLoginService} from "./services/social-login.service";
 import {AlertComponent} from "./components/alert/alert.component";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatInputModule} from "@angular/material/input";
 import {ScrollingModule} from "@angular/cdk/scrolling";
-import {MatDialogModule} from "@angular/material/dialog";
-import {MatAutocompleteModule} from "@angular/material/autocomplete";
-import {MatBadgeModule} from "@angular/material/badge";
-import {MatBottomSheetModule} from "@angular/material/bottom-sheet";
-import {MatButtonModule} from "@angular/material/button";
-import {MatButtonToggleModule} from "@angular/material/button-toggle";
-import {MatCheckboxModule} from "@angular/material/checkbox";
-import {MatChipsModule} from "@angular/material/chips";
-import {MatStepperModule} from "@angular/material/stepper";
-import {MatDatepickerModule} from "@angular/material/datepicker";
-import {MatDividerModule} from "@angular/material/divider";
-import {MatExpansionModule} from "@angular/material/expansion";
-import {MatGridListModule} from "@angular/material/grid-list";
-import {MatNativeDateModule, MatRippleModule} from "@angular/material/core";
-import {MatPaginatorModule} from "@angular/material/paginator";
-import {MatProgressBarModule} from "@angular/material/progress-bar";
-import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
-import {MatRadioModule} from "@angular/material/radio";
-import {MatSelectModule} from "@angular/material/select";
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatSliderModule} from "@angular/material/slider";
-import {MatSlideToggleModule} from "@angular/material/slide-toggle";
-import {MatSnackBarModule} from "@angular/material/snack-bar";
-import {MatSortModule} from "@angular/material/sort";
-import {MatTableModule} from "@angular/material/table";
-import {MatTabsModule} from "@angular/material/tabs";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatTooltipModule} from "@angular/material/tooltip";
-import {MatTreeModule} from "@angular/material/tree";
 import {OverlayModule} from "@angular/cdk/overlay";
 import {PortalModule} from "@angular/cdk/portal";
-import {MatCardModule} from "@angular/material/card";
-import {MatMenuModule} from "@angular/material/menu";
-import {MatListModule} from "@angular/material/list";
-import {MatIconModule} from "@angular/material/icon";
 import {ClipboardModule} from "@angular/cdk/clipboard";
 import {CdkStepperModule} from "@angular/cdk/stepper";
 import {CdkTableModule} from "@angular/cdk/table";
 import {CdkTreeModule} from "@angular/cdk/tree";
 import {DragDropModule} from "@angular/cdk/drag-drop";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MaterialModule} from "./material.module";
 
 @NgModule({
   declarations: [
@@ -85,33 +51,7 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
     DragDropModule,
     HttpClientModule,
     FormsModule,
-    //MaterialModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatBottomSheetModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatCardModule,
-    MatCheckboxModule,
-    MatStepperModule,
-    MatDialogModule,
-    MatDividerModule,
-    MatExpansionModule,
-    MatGridListModule,
-    MatIconModule,
-    MatListModule,
-    MatMenuModule,
-    MatRadioModule,
-    MatRippleModule,
-    MatSelectModule,
-    MatSidenavModule,
-    MatSliderModule,
-    MatSlideToggleModule,
-    MatTableModule,
-    MatTabsModule,
-    MatToolbarModule,
-    MatTooltipModule,
-    MatTreeModule,
+    MaterialModule,
     NgbModule,
     OverlayModule,
     PortalModule,

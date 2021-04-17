b336defa520807e74eaf5a6fe99ba4ac31a1a0a7
gimartinez <gm@famly.co>
2021-04-18 00:57:47 +0200

    :lock: test auth data

--- etc/mysql/init.sql
+++ etc/mysql/init.sql
@@ -63,6 +63,152 @@ VALUES ('shopping-mall', 'Example client. Fixed to look like a mall');
 INSERT INTO client
 VALUES ('ikeallabea-shoppingmall', 'Example client. Fixed to look like an IKEA');
 
+
+INSERT INTO client_auth
+VALUES ('f498fe98-cc9f-4691-8216-cc98000fc789', 'hotel-1', 'survey', '
+{
+  "surveyForm": {
+    "id": "hotel-1",
+    "title": "Podes obtener conexión a internet de manera gratuita, si nos ayudás con la siguiente encuesta",
+    "fields": [
+      {
+        "id": "1571507840",
+        "type": "textbox",
+        "config": {
+          "key": "textbox",
+          "otherOptions": {
+            "textType": "text"
+          },
+          "label": "Lo que quieras describir de nuestro hotel",
+          "required": true,
+          "value": "Recepción, bar, etc",
+          "order": 1
+        }
+      },
+      {
+        "id": "1571507819",
+        "type": "rating",
+        "config": {
+          "key": "rating",
+          "label": "¿Cómo calificaría la atención recibida al registrarse en el hotel (check in)?",
+          "otherOptions": {
+            "maximum": 5
+          },
+          "order": 2
+        }
+      },
+      {
+        "id": "1571508048",
+        "type": "radio",
+        "config": {
+          "key": "yes-no",
+          "label": "Volverias a elegirnos para hospedarte?",
+          "otherOptions": [
+            {
+              "key": "Sí"
+            },
+            {
+              "key": "No sé"
+            },
+            {
+              "key": "Puede ser .."
+            }
+          ],
+          "order": 3
+        }
+      },
+      {
+        "id": "1571507880",
+        "type": "textbox",
+        "config": {
+          "key": "textbox",
+          "otherOptions": {
+            "textType": "text"
+          },
+          "label": "Queres dejarnos alguna sugerencia?",
+          "value": "..",
+          "required": false,
+          "order": 4
+        }
+      },
+      {
+        "id": "1571508821",
+        "type": "textbox",
+        "config": {
+          "key": "textbox",
+          "otherOptions": {
+            "textType": "text"
+          },
+          "label": "Como nos encontraste?",
+          "required": false,
+          "value": "Redes sociales",
+          "order": 5
+        }
+      },
+      {
+        "id": "1571508049",
+        "type": "checkbox",
+        "config": {
+          "label": "El vestido era de color",
+          "otherOptions": [
+            {
+              "index": 0,
+              "key": "Azul"
+            },
+            {
+              "index": 1,
+              "key": "Dorado"
+            },
+            {
+              "index": 2,
+              "key": "No sé"
+            }
+          ],
+          "order": 6
+        }
+      },
+      {
+        "id": "1617627751",
+        "type": "selector",
+        "config": {
+          "label": "Volverias a elegirnos?",
+          "otherOptions": [
+            {
+              "key": "Azul"
+            },
+            {
+              "key": "No sé"
+            },
+            {
+              "key": "Puede ser .."
+            }
+          ],
+          "order": 7
+        }
+      }
+    ]
+  }
+}');
+
+INSERT INTO client_auth
+VALUES ('b56b48aa-3fda-41a0-8608-d14209a6a956', 'shopping-mall', 'social-login', '
+{
+  "socialMediaKeys": {
+    "facebook": {
+      "clientId": "131065570894352",
+      "secret": "a74151d55bae152570b3a0e8874086db"
+    },
+    "google": {
+      "clientId": "500143808314-9psv199snl4g7e6dargf6f8sog0023u1.apps.googleusercontent.com",
+      "secret": "8oJHj4r0tCWBxJ_wTFNBOtD2"
+    },
+    "linkedin": {
+      "clientId": "7795dtgk291ni1",
+      "secret": "oBU1OUVw42FB7MWt"
+    }
+  }
+}');
+
 INSERT INTO client_landing
 VALUES ('83bfedd1-5467-408c-86d3-146e1d9275ed', 'hotel-1', 'template-2', '
 {
